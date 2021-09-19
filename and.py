a06178ba50d2f3586e8f8814f494ba69a7e1e8da
Chamarthidheeraj <dheerajchamarthi@gmail.com>
2021-09-19 13:29:19 +0530

    completed

--- and.py
+++ and.py
@@ -1,5 +1,5 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data,save_model,save_plot
 import pandas as pd
 import numpy as np
 
@@ -11,7 +11,7 @@ AND = {
 
 df = pd.DataFrame(AND)
 
-df
+print(df)
 
 
 X,y = prepare_data(df)
@@ -24,3 +24,5 @@ model.fit(X, y)
 
 _ = model.total_loss()
 #final model 
+save_model(model,filename='and.model')
+save_plot(df,"and.png", model)
